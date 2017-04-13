0f31c0f2186525bd64c5437eb3ab727f418d80fc
HenryZhao <zsl.zju@gmail.com>
2017-04-13 22:28:17 +0800

    update model.py in shenglin1987

--- cdmemnn/model.py
+++ cdmemnn/model.py
@@ -1,118 +1,187 @@
-import mxnet as mx
 import numpy as np
+import mxnet as mx
 import random
-from collections import namedtuple
+import sys
+import logging
+import argparse
+import collections
+import model,os
+class Batch:
+    def __init__(self, data_names, data, label_names, label, pad=0):
+        self.data=data
+        self.data_names=data_names
+        self.label=label
+        self.label_names=label_names
 
-GRUState=namedtuple('GRUState', ['h'])
-GRUParam=namedtuple('GRUParam', ['gates_i2h_weight','gates_i2h_bias', 'gates_h2h_weight', 'gates_h2h_bias','trans_i2h_weight','trans_i2h_bias', 'trans_h2h_weight', 'trans_h2h_bias'])
+    def provide_data(self):
+        return [(n, x.shape) for n, x in zip(self.data_names, self.data)]
 
-
-def myGRU(num_hidden, indata, prev_state, param, seqidx, layeridx, dropout=0.):
-    if dropout>0.:
-        indata=mx.sym.Dropout(data=indata, p=dropout)
-
-    i2h=mx.sym.FullyConnected(data=indata, weight=param.gates_i2h_weight, bias=param.gates_i2h_bias, num_hidden=num_hidden*2, name='t%d_l%d_gates_i2h'%(seqidx, layeridx))
-    h2h=mx.sym.FullyConnected(data=prev_state.h, weight=param.gates_h2h_weight, bias=param.gates_h2h_bias, num_hidden=num_hidden*2, name='t%d_l%d_gates_h2h'%(seqidx, layeridx))
-
-    gates=i2h+h2h
-
-    slice_gates=mx.sym.SliceChannel(gates, num_outputs=2, name='t%d_l%d_slice'%(seqidx, layeridx))
-
-    update_gate=mx.sym.Activation(data=slice_gates[0], act_type='sigmoid')
-    reset_gate=mx.sym.Activation(data=slice_gates[1], act_type='sigmoid')
-
-    htrans_i2h=mx.sym.FullyConnected(data=indata, weight=param.trans_i2h_weight, bias=param.trans_i2h_bias, num_hidden=num_hidden, name='t%d_l%d_trans_i2h'%(seqidx, layeridx))
+    def provide_label(self):
+        return [(n,x.shape) for n, x in zip(self.label_names, self.label)]
 
 
-    h_after_reset=prev_state.h*reset_gate
-    htrans_h2h=mx.sym.FullyConnected(data=h_after_reset, weight=param.trans_h2h_weight, bias=param.trans_h2h_bias, num_hidden=num_hidden, name='t%d_l%d_trans_h2h'%(seqidx, layeridx))
+class DataIter(mx.io.DataIter):
+    def __init__(self, data, batch_size, user2item, item2user, upass, ipass):
+        super(DataIter, self).__init__()
+        self.batch_size=batch_size
+        self.upass=upass
+        self.ipass=ipass
+        self.data=data
+        self.user2item=user2item
+        self.item2user=item2user
+        self.provide_data=[('user',(self.batch_size,)), ('item', (self.batch_size,)), ('grp_u', (self.batch_size,upass)), ('grp_i', (self.batch_size,ipass)),]
+        self.provide_label = [('rating', (self.batch_size,)),]
 
-    htrans=htrans_i2h+htrans_h2h
+    def __iter__(self):
+        for k in xrange(len(self.data)/self.batch_size):
+            users=[]
+            items=[]
+            scores=[]
+            grp_u=[]
+            grp_i=[]
+           #Generate each batch data and yield the result
+            for i in xrange(self.batch_size):
+                j=k*self.batch_size+i
+                user, item, score=self.data[j]
+                users.append(user)
+                items.append(item)
+                scores.append(score)
+                ug=list(item2user[item])+[user]*(upass-len(item2user[item])) if upass>len(item2user[item]) else random.sample(item2user[item], upass)
+                ig=list(user2item[user])+[item]*(ipass-len(user2item[user])) if ipass>len(user2item[user]) else random.sample(user2item[user], ipass)
+                grp_u.append(ug)
+                grp_i.append(ig)
+                    
+            data_all=[mx.nd.array(users), mx.nd.array(items), mx.nd.array(grp_u), mx.nd.array(grp_i)]
+            label_all=[mx.nd.array(scores)]
+            data_names=['user', 'item', 'grp_u', 'grp_i',]
+            label_names=['rating',]
 
-    htrans_act=mx.sym.Activation(data=htrans, act_type='tanh')
+            data_batch=Batch(data_names, data_all, label_names, label_all)
+            yield data_batch
 
-    next_h=prev_state.h+update_gate*(htrans_act-prev_state.h)
-    return GRUState(h=next_h)
+    def reset(self):
+        random.shuffle(self.data)
 
-def get_cdnn(batch_size, num_embed, num_hidden, num_layer, num_user, num_item, nupass, nipass, npass, dropout=0.):
-    last_states=[]
-    param_cells=[]
-    for i in xrange(num_layer):
-        param_cells.append(GRUParam(gates_i2h_weight=mx.sym.Variable('l%d_i2h_gates_weight'%i), gates_i2h_bias=mx.sym.Variable('l%d_i2h_gates_bias'%i),gates_h2h_weight=mx.sym.Variable('l%d_h2h_gates_weight'%i),gates_h2h_bias=mx.sym.Variable('l%d_h2h_gates_bias'%i),trans_i2h_weight=mx.sym.Variable('l%d_i2h_trans_weight'%i), trans_i2h_bias=mx.sym.Variable('l%d_i2h_bias'%i), trans_h2h_weight=mx.sym.Variable('l%d_h2h_trans_weight'%i), trans_h2h_bias=mx.sym.Variable('l%d_h2h_bias'%i)))
-        state=GRUState(h=mx.sym.Variable('l%d_init_h'%i))
-        last_states.append(state)
-    user=mx.sym.Variable('user')
-    item=mx.sym.Variable('item')
-    rating=mx.sym.Variable('rating')
-    grp_u=mx.sym.Variable('grp_u')
-    grp_i=mx.sym.Variable('grp_i')
+def RMSE(label, pred):
+    ret=0.0
+    n=0.0
+    pred=pred.flatten()
+    for i in xrange(len(label)):
+        ret+=(label[i]-pred[i])**2
+        n+=1.0
+    return np.sqrt(ret/n)
 
-    weight_u=mx.sym.Variable('u_weight')
-    weight_i=mx.sym.Variable('i_weight')
+def get_data(data, split, batch_size, user2item, item2user, upass, ipass):
+    tr=random.sample(data, int(split*len(data)))
+    te=list(set(data)-set(tr))
 
-    #weight_uu=mx.sym.Variable('aff_u', shape=(num_embed, num_embed), init=mx.initializer.Xavier(rnd_type='gaussian', factor_type='avg', magnitude=2))
-    #weight_ii=mx.sym.Variable('aff_i', shape=(num_embed, num_embed), init=mx.initializer.Xavier(rnd_type='gaussian', factor_type='avg', magnitude=2))
+    return (DataIter(tr, batch_size, user2item, item2user, upass, ipass), DataIter(te, batch_size, user2item, item2user, upass, ipass))
 
-    weight_z=mx.sym.Variable('z_weight')
-    bias_z=mx.sym.Variable('z_bias')
-    m_u=mx.sym.Embedding(data=user, input_dim=num_user, output_dim=num_embed, weight=weight_u)
-    m_i=mx.sym.Embedding(data=item, input_dim=num_item, output_dim=num_embed, weight=weight_i)
+def train(args,data_path, data, val, split, network, batch_size, num_epoch, user2item, item2user, upass, ipass, learning_rate, logname):
+    ''' 
+    model=mx.model.FeedForward(ctx=mx.gpu(),symbol=network, num_epoch=num_epoch,learning_rate=learning_rate, wd=0.0001, momentum=0.9, initializer=mx.init.Normal(sigma=0.01))
+    train, test= get_data(data, batch_size, user2item, item2user,  upass, ipass)
+    logging.basicConfig(level=logging.DEBUG)
+    model.fit(train,eval_data=test, eval_metric=RMSE, batch_end_callback=mx.callback.Speedometer(batch_size, 20000/batch_size))
+    '''
+    network=mx.mod.Module(network, data_names=('user', 'item', 'grp_u', 'grp_i',), label_names=('rating',), context=mx.gpu())
+    #network.bind(data_shapes=[('user', (batch_size,)), ('item', (batch_size,)), ('grp_u', (batch_size,)), ('grp_i', (batch_size,)),], label_shapes=[('rating', (batch_size,)),])
 
-    grp_u= mx.sym.Embedding(data=grp_u, input_dim=num_user, output_dim=num_embed, weight=weight_u)
-    grp_i= mx.sym.Embedding(data=grp_i, input_dim=num_item, output_dim=num_embed, weight=weight_i)
+    #init=mx.init.Xavier(factor_type='in', magnitude=1)
+    #network.init_params(initializer=init)
+    #network.init_optimizer(optimizer='adam', kvstore=None, optimizer_params={'learning_rate':1E-3, 'wd':1E-4}) 
+    if not args.val:
+        train, val_iter= get_data(data, split, batch_size, user2item, item2user,  upass, ipass)
+    else:
+        train=DataIter(data, batch_size, user2item, item2user, upass, ipass)
+        val_iter=DataIter(val, batch_size, user2item, item2user, upass, ipass)
+    if args.log:
+        logging.basicConfig(filename=os.path.join('/'.join(data_path.split('/')[:-1]+['res']), logname+'.log'), level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
+    logging.info('start with arguments %s', args)
+    network.fit(train, eval_data=val_iter, eval_metric=RMSE, optimizer_params={'learning_rate':learning_rate, 'momentum':0.85}, num_epoch=num_epoch, batch_end_callback=mx.callback.Speedometer(batch_size, 500))
+    print 'another train'
+    network.fit(train, eval_data=val_iter, eval_metric=RMSE, optimizer_params={'learning_rate':learning_rate, 'momentum':0.85}, num_epoch=num_epoch, batch_end_callback=mx.callback.Speedometer(batch_size, 500))
+    '''
+    for i in xrange(num_epoch):
+        batch_data=random.sample(data, batch_size)
+        users=[]
+        items=[]
+        scores=[]
+        grp_u=[]
+        grp_i=[]
+        #Generate each batch data and yield the result
+        for x in batch_data:
+            user, item, score=x
+            users.append(user)
+            items.append(item)
+            scores.append(score)
+            ug=list(item2user[item])+[user]*(upass-len(item2user[item])) if upass>len(item2user[item]) else random.sample(item2user[item], upass)                       ig=list(user2item[user])+[item]*(ipass-len(user2item[user])) if ipass>len(user2item[user]) else random.sample(user2item[user], ipass)
+            grp_u.append(ug)
+            grp_i.append(ig)
+        network.forward(data_batch=mx.io.D)
+    '''
+def convert_data(user, item, score, user_dict, item_dict, user2item, item2user, is_train):
+    if user not in user_dict:
+        user_dict[user]=len(user_dict)
+    user=user_dict[user]
+    if item not in item_dict:
+        item_dict[item]=len(item_dict)
+    item=item_dict[item]
+    user2item[user].add(item)
+    item2user[item].add(user)
+    return (user, item, float(score))
 
-    col_u=mx.sym.SliceChannel(data=grp_u, axis=1, num_outputs=nupass, squeeze_axis=1)
-    col_i=mx.sym.SliceChannel(data=grp_i, axis=1, num_outputs=nipass, squeeze_axis=1)
-    for _ in xrange(npass):
-        for i in xrange(nupass):
-            cur_col_u=col_u[i]
-            q_u=mx.sym.Embedding(data=user, input_dim=num_user, output_dim=num_embed, weight=weight_u)
-            z=[cur_col_u, m_u, q_u, mx.sym.abs(q_u-m_u), mx.sym.abs(q_u-cur_col_u), mx.sym.abs(cur_col_u-m_u)]
-            z=mx.sym.Concat(*z, dim=1)
-            z=mx.sym.FullyConnected(data=z, num_hidden=num_hidden,weight=weight_z, bias=bias_z, name='ufc1')
-            z=mx.sym.Activation(data=z, act_type='relu')
-            z=mx.sym.FullyConnected(data=z, num_hidden=num_hidden, name='ufc2')
-            z=mx.sym.Activation(data=z, act_type='relu')
-            z=mx.sym.FullyConnected(data=z, num_hidden=nupass)
-            g=mx.sym.SoftmaxActivation(data=z)
-            c_u=mx.sym.broadcast_mul(grp_u, g)
-            c_u=mx.sym.sum_axis(data=c_u, axis=1)
-            mu_state=GRUState(h=m_u)
-            next_state=myGRU(num_embed, indata=cur_col_u, prev_state=mu_state, param=param_cells[0], seqidx=0, layeridx=0, dropout=dropout)
-            m_u=next_state.h
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('-train', help='Data file', dest='fi', required=True)
+    parser.add_argument('-nhidden', help='num of hidden', dest='num_hidden', default=50)
+    parser.add_argument('-nembed', help='num of embedding', dest='num_embed', default=50)
+    parser.add_argument('-batch_size', help='batch size', dest='batch_size', default=500)
+    parser.add_argument('-nepoch', help='num of epoch', dest='num_epoch', default=100)
+    parser.add_argument('-upass', help='num of collaborative users', dest='upass', default=5)
+    parser.add_argument('-ipass', help='num of collaborative items', dest='ipass', default=5)
+    parser.add_argument('-npass', help='num of collaborative passes', dest='npass', default=1)
+    parser.add_argument('-nlayer', help='num of GRU layers', dest='num_layer', default=1)
+    parser.add_argument('-eta', help='learning rate', dest='learning_rate', default=0.01)
+    parser.add_argument('-dropout', help='dropout', dest='dropout', default=0.2)
+    parser.add_argument('-split',dest='split', help='train & test split ratio', default=0.9)
+    parser.add_argument('-val', help='validation set', dest='val', default=None)
+    parser.add_argument('-log', help='log to file', default=False)
+    args=parser.parse_args()
 
+    user2item=collections.defaultdict(set)
+    item2user=collections.defaultdict(set)
 
-            
-        for i in xrange(nipass):
-            cur_col_i=col_i[i]
-            q_i=mx.sym.Embedding(data=item, input_dim=num_item, output_dim=num_embed, weight=weight_i)
-            z=[cur_col_i, m_i, q_i, mx.sym.abs(q_i-m_i), mx.sym.abs(q_i-cur_col_i), mx.sym.abs(cur_col_i-m_i)]
-            z=mx.sym.Concat(*z, dim=1)
-            z=mx.sym.FullyConnected(data=z, num_hidden=num_hidden,weight=weight_z, bias=bias_z, name='ifc1')
-            z=mx.sym.Activation(data=z, act_type='relu')
-            z=mx.sym.FullyConnected(data=z, num_hidden=num_hidden, name='ifc2')
-            z=mx.sym.Activation(data=z, act_type='relu')
-            z=mx.sym.FullyConnected(data=z, num_hidden=nupass)
-            g=mx.sym.SoftmaxActivation(data=z)
-            c_i=mx.sym.broadcast_mul(grp_i, g)
-            c_i=mx.sym.sum_axis(data=c_i, axis=1)
-            mi_state=GRUState(h=m_i)
-            next_state=myGRU(num_embed, indata=cur_col_i,prev_state=mi_state, param=param_cells[0], seqidx=0, layeridx=0, dropout=dropout)
-            m_i=next_state.h
-    m_u=mx.sym.Concat(m_u, mx.sym.Embedding(data=user, input_dim=num_user, output_dim=num_embed, weight=weight_u), dim=1)
-    m_i=mx.sym.Concat(m_i, mx.sym.Embedding(data=item, input_dim=num_item, output_dim=num_embed, weight=weight_i), dim=1)
-    pred=m_u*m_i
-    pred=mx.sym.sum_axis(data=pred, axis=1)
-    pred=mx.sym.Flatten(data=pred)
-    pred=mx.sym.LinearRegressionOutput(data=pred, label=rating)
-    return pred
-
-
-        
-
-
-
-
-
-
-
+    user_dict={}
+    item_dict={}
+    data=[]
+    with open(args.fi, 'r') as f:
+        for line in f:
+            tks=line.strip().split('\t')
+            data.append(convert_data(int(tks[0]), int(tks[1]), float(tks[2]), user_dict, item_dict, user2item, item2user, True))
+    val=[]
+    if args.val:
+        with open(args.val, 'r') as f:
+            for line in f:
+                tks=line.strip().split('\t')
+                if int(tks[0]) not in user_dict or int(tks[1]) not in item_dict:
+                    continue
+                val.append(convert_data(int(tks[0]), int(tks[1]), float(tks[2]), user_dict, item_dict, user2item, item2user, False))
+    print '#Users, ',len(user_dict)
+    print '#Items, ',len(item_dict)
+    print '#Ratings, ', len(data)
+    num_hidden=int(args.num_hidden)
+    batch_size=int(args.batch_size)
+    num_epoch=int(args.num_epoch)
+    learning_rate=float(args.learning_rate)
+    num_embed=int(args.num_embed)
+    num_layer=int(args.num_layer)
+    upass=int(args.upass)
+    ipass=int(args.ipass)
+    npass=int(args.npass)
+    split=float(args.split)
+    net=model.get_cdnn(batch_size, num_embed, num_hidden, num_layer, len(user_dict), len(item_dict), upass, ipass, npass, float(args.dropout))
+    logname='upass_'+str(args.upass)+'_ipass_'+str(args.ipass)+"_embed_"+str(args.num_embed)+'_hidden_'+str(args.num_hidden)+"_eta_"+str(args.learning_rate)+"_batch_size_"+str(args.batch_size)
+    train(args, args.fi,data, val, split, net, batch_size, num_epoch, user2item, item2user, upass, ipass, learning_rate, logname)
